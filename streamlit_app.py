f2ec397297eaf237fccff5a2c62f311f85ef8e58
Sushmithakm789 <136970266+Sushmithakm789@users.noreply.github.com>
2023-06-18 17:08:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text(' 🐔 Hard-Boiled Free-Tange Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
 import pandas
-my_fruit_list =  (pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt").
-streamlit.dataframe(my_fruit_list)                 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)                
